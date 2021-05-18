662767988e7dda9f5c51a73e57832cf1164f71dc
fistadev <fabiofistarol@gmail.com>
2021-05-18 22:38:42 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# site title
+st.title("Tracker App")
+st.subheader(
+    "Strive School - Google Fit Project")
