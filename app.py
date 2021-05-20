7cf28868ffbf851610611e42e3aaf90501879260
fistadev <fabiofistarol@gmail.com>
2021-05-20 15:35:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import plotly.express as px
 from PIL import Image
 import os
-# import pickle
+import pickle
 
 from sklearn import set_config
 # accuracy_score, balanced_accuracy_score, plot_confusion_matrix
@@ -18,6 +18,21 @@ from sklearn import impute
 from sklearn.preprocessing import RobustScaler
 from sklearn import preprocessing  # OrdinalEncoder, LabelEncoder
 from sklearn import pipeline      # Pipeline
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import RobustScaler
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+# Necesary for HistGradientBoostingClassifier
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 sns.set_style("whitegrid")
@@ -36,10 +51,16 @@ st.markdown(
 )
 
 
+# header=st.beta_container()
+# team=st.beta_container()
+# dataset=st.beta_container()
+# footer=st.beta_container()
+
+
 # Load Data
 @st.cache(allow_output_mutation=True)
 def load_data(filename=None):
-    filename_default = './data/archive/dataset_5secondWindow%5B1%5D.csv'
+    filename_default = './data/dataset_halfSecondWindow.csv'
     if not filename:
         filename = filename_default
 
@@ -47,12 +68,25 @@ def load_data(filename=None):
     return df
 
 
-dataset = load_data()
+df = load_data()
 
-# load_clf = pickle.load(open('./data/model.pkl', "rb"))
+###########
+# pickle
+
+# save the model to disk
+# filename = './data/final_model_v2.sav'
+# pickle.dump(model, open(filename, 'wb'))
+
+# # load the model from disk
+# loaded_model = pickle.load(open(filename, 'rb'))
+# result = loaded_model.score(x_test, y_test)
+# st.write(result)
+
+############
+
 
 # remove issues from plots
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # site title
 st.title("Tracker App")
@@ -68,142 +102,140 @@ st.text(" ")
 ########
 
 # input predictions for streamlit
-gender = st.selectbox("Select your gender: ", options=['Male', 'Female'])
-weight = st.slider("Enter your weight (kg): ", 1, 200, 1)
-height = st.text_input('Enter heigth (m):')
+# gender = st.selectbox("Select your gender: ", options=['Male', 'Female'])
+# weight = st.slider("Enter your weight (kg): ", 1, 200, 1)
+# height = st.text_input('Enter heigth (m):')
 age = st.slider("Enter your age:", 1, 100, 1)
-heart_rate = st.slider("Enter your heart rate (bpm): ", 1, 200, 1)
-time = st.slider('Enter time (min): ', 1, 300, 1)
+# heart_rate = st.slider("Enter your heart rate (bpm): ", 1, 200, 1)
 
-
-########
-
-# drop columns
-
-to_drop = [i for i in dataset.columns if 'std' in i]
-dataset.drop(to_drop, axis=1, inplace=True)
-to_drop = [i for i in dataset.columns if 'accelerometer_max' in i]
-dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'gyroscope_max' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'gyroscope_min' in i]
-dataset.drop(to_drop, axis=1, inplace=True)
-to_drop = [i for i in dataset.columns if 'sound_min' in i]
-dataset.drop(to_drop, axis=1, inplace=True)
-to_drop = [i for i in dataset.columns if 'sound_max' in i]
-dataset.drop(to_drop, axis=1, inplace=True)
-
-########
-
-# clean
-dataset.columns = dataset.columns.str.replace(
-    'android.sensor.', '').str.replace('#', '_')
-
-
-# print head of the dataset
-# st.write("Dataset")
-# st.table(dataset.head(5))
-
+st.write('Your age is: ', age)
 
 ########
 
 # st.markdown("""---""")
 
-# checking distribution by different sensors (time, accelerometer, gyroscope, sound)
+# st.echo()
+# with st.echo():
+#     # merge car and bus
+#     vehicle_dict = {'Car': 'Vehicle', 'Bus': 'Vehicle',
+#                     'Train': 'Vehicle', 'Walking': 'Walking'}
+#     dataset.replace({'target': vehicle_dict}, inplace=True)
 
-# st.subheader("Plotting")
-# st.write(" ")
 
-# plt.figure(figsize=(10, 15))
+#######
 
-# plt.subplot(4, 1, 1)
-# sns.distplot(dataset.iloc[:, 0])
-# plt.xlabel('Time')
+# Clean column names
+df.columns = df.columns.str.replace(
+    'android.sensor.', '').str.replace('#', '_')
 
-# plt.subplot(4, 1, 2)
-# for i in range(1, 4):
-#     sns.distplot(dataset.iloc[:, i])
-# plt.legend(dataset.iloc[:, 1:4].columns)
-# plt.xlabel('Accelerometer')
+# feature engineering
+df['acc_mean*gyro_mean'] = df['accelerometer_mean'] * df['gyroscope_mean']
+df['acc_mean*sound_mean'] = df['accelerometer_mean'] * df['sound_mean']
 
-# plt.subplot(4, 1, 3)
-# for i in range(4, 7):
-#     sns.distplot(dataset.iloc[:, i])
-# plt.legend(dataset.iloc[:, 4:7].columns)
-# plt.xlabel('Gyroscope')
+df['rv_gyro_mean'] = df['rotation_vector_mean'] * df['gyroscope_mean']
+df['lin_speed_mean'] = df['linear_acceleration_mean'] * df['speed_mean']
+df['rv_gyro_prox_mean'] = df['rotation_vector_mean'] * \
+    df['gyroscope_mean'] * df['proximity_std']
+df['lin_speed_prox_mean'] = df['linear_acceleration_mean'] * \
+    df['speed_mean'] * df['proximity_std']
+df['rv_gyro__grv_mean'] = df['rotation_vector_mean'] * \
+    df['gyroscope_mean'] * df['game_rotation_vector_mean']
 
-# plt.subplot(4, 1, 4)
-# for i in range(7, 10):
-#     sns.distplot(dataset.iloc[:, i])
-# plt.legend(dataset.iloc[:, 7:10].columns)
-# plt.xlabel('Sound')
+vehicle_dict = {'Car': 'Vehicle', 'Bus': 'Vehicle',
+                'Train': 'Vehicle', 'Still': 'Still', 'Walking': 'Walking'}
+df.replace({'target': vehicle_dict}, inplace=True)
 
-# st.pyplot()
+########
+
+df = df[['accelerometer_mean',
+         'accelerometer_min',
+         'accelerometer_max',
+         'accelerometer_std',
+         'linear_acceleration_mean', 'linear_acceleration_min', 'linear_acceleration_max', 'linear_acceleration_std',
+         'orientation_mean',
+         'orientation_min',
+         'orientation_max',
+         'orientation_std',
+         'magnetic_field_mean',
+         'magnetic_field_min',
+         'magnetic_field_max',
+         'magnetic_field_std',
+         'gyroscope_mean',
+         'gyroscope_min',
+         'gyroscope_max',
+         'gyroscope_std',
+         'gravity_mean',
+         'gravity_min',
+         'gravity_max',
+         'gravity_std',
+         'acc_mean*gyro_mean',
+         'acc_mean*sound_mean',
+         'user',
+         'target'
+         ]]
+
+
+df.fillna(0, inplace=True)
+
+########
+
+# train test split
+big_users = ['U1', 'U3', 'U6', 'U7', 'U10', 'U12']
+
+train_df = df[df.user.isin(big_users)]
+test_df = df[~df.user.isin(big_users)]
+
+
+train_df.drop('user', axis=1, inplace=True)
+test_df.drop('user', axis=1, inplace=True)
+
+
+num_vars = [
+    'accelerometer_mean', 'accelerometer_min', 'accelerometer_max', 'accelerometer_std',
+    'linear_acceleration_mean', 'linear_acceleration_min', 'linear_acceleration_max', 'linear_acceleration_std',
+    'magnetic_field_mean', 'magnetic_field_min', 'magnetic_field_max', 'magnetic_field_std',
+    'gyroscope_mean', 'gyroscope_min', 'gyroscope_max', 'gyroscope_std',
+    'orientation_mean', 'orientation_min', 'orientation_max', 'orientation_std',
+    'gravity_mean', 'gravity_min', 'gravity_max', 'gravity_std',
+    'acc_mean*gyro_mean', 'acc_mean*sound_mean']
 
 
 ########
 
-# st.subheader("Plotting")
-# st.write(" ")
-
-# sns.set(style="ticks")
-# sns.pairplot(data=dataset.loc[:, :"target"], hue="target")
-# st.pyplot()
+# split
+x_train = train_df[num_vars]
+x_test = test_df[num_vars]
+y_train = train_df.target
+y_test = test_df.target
 
 
 ########
 
-st.markdown("""---""")
-
-st.subheader("Correlation Matrix")
-st.write(" ")
-
-# correlation matrix
-fig = px.imshow(dataset.corr())
-st.plotly_chart(fig)
+# load the model from disk
+filename = './data/final_model_v2.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+result = (loaded_model.score(x_test, y_test) * 100)
+# st.write(result)
 
 
 ##########
 
-st.markdown("""---""")
+# st.markdown("""---""")
 
-st.subheader("Upload your data")
-st.write(" ")
+# st.subheader("Upload your data")
+# st.write(" ")
 
-st.file_uploader('File uploader')
+# st.file_uploader('File uploader')
 
-
-def file_selector(folder_path='./data'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-
-filename_new = file_selector()
-st.write('You selected `%s`' % filename_new)
-
-# new dataset
-df_new = pd.read_csv(f"./{filename_new}")
-
-# drop from new dataset
-# to_drop = [i for i in dataset.columns if 'std' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'gyroscope_min' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'gyroscope_max' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'accelerometer_min' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-# to_drop = [i for i in dataset.columns if 'accelerometer_max' in i]
-# dataset.drop(to_drop, axis=1, inplace=True)
-
-# print head of the new dataset
-st.write("Dataset")
-st.table(df_new.head(5))
 
 #######
 
-# st.write(" ")
-# st.write(" ")
+st.write(" ")
+st.write(" ")
+# pred_button = st.checkbox("Check prediction")
+# if pred_button:
+#     st.checkbox(pred, value=True)
 
-# st.button('Hit me')
+if st.button('Check prediction'):
+    st.write('result: %s' % result, '%')
+    # st.write(result)
